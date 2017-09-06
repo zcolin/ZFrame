081050ac19da573fb7ffe0e06d5843dc4f11f6a9
colin <wanglin2046@126.com>
2017-09-06 16:53:18 +0800

    # 增加HtmlUtil类用来转换Html和string

--- libFrame/src/main/java/com/zcolin/frame/util/AndroidVersionUtil.java
+++ libFrame/src/main/java/com/zcolin/frame/util/AndroidVersionUtil.java
@@ -1,9 +1,11 @@
-/***********************************************************
- * author   colin
- * company  fosung
- * email    wanglin2046@126.com
- * date     16-7-15 下午4:41
- **********************************************************/
+/*
+ * *********************************************************
+ *   author   colin
+ *   company  fosung
+ *   email    wanglin2046@126.com
+ *   date     17-9-6 下午4:52
+ * ********************************************************
+ */
 package com.zcolin.frame.util;
 
 import android.os.Build;
@@ -16,63 +18,7 @@ public class AndroidVersionUtil {
     }
 
     /**
-     * 当前Android系统版本是否在（ Donut） Android 1.6或以上
-     *
-     * @return
-     */
-    public static boolean hasDonut() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.DONUT;
-    }
-
-    /**
-     * 当前Android系统版本是否在（ Eclair） Android 2.0或 以上
-     *
-     * @return
-     */
-    public static boolean hasEclair() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR;
-    }
-
-    /**
-     * 当前Android系统版本是否在（ Froyo） Android 2.2或 Android 2.2以上
-     *
-     * @return
-     */
-    public static boolean hasFroyo() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
-    }
-
-    /**
-     * 当前Android系统版本是否在（ Gingerbread） Android 2.3x或 Android 2.3x 以上
-     *
-     * @return
-     */
-    public static boolean hasGingerbread() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
-    }
-
-    /**
-     * 当前Android系统版本是否在（ Honeycomb） Android3.1或 Android3.1以上
-     *
-     * @return
-     */
-    public static boolean hasHoneycomb() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
-    }
-
-    /**
-     * 当前Android系统版本是否在（ HoneycombMR1） Android3.1.1或 Android3.1.1以上
-     *
-     * @return
-     */
-    public static boolean hasHoneycombMR1() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1;
-    }
-
-    /**
      * 当前Android系统版本是否在（ IceCreamSandwich） Android4.0或 Android4.0以上
-     *
-     * @return
      */
     public static boolean hasIcecreamsandwich() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
@@ -80,8 +26,6 @@ public class AndroidVersionUtil {
 
     /**
      * 当前android系统版本是否在（JellyBean）Android4.1或android4.1以上
-     *
-     * @return
      */
     public static boolean hasJellyBean() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
@@ -89,8 +33,6 @@ public class AndroidVersionUtil {
 
     /**
      * 当前android系统版本是否在（KitKat）Android4.4或android4.4以上
-     *
-     * @return
      */
     public static boolean hasKitKat() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
@@ -106,9 +48,14 @@ public class AndroidVersionUtil {
     /**
      * 当前是否在6.0以上
      */
-    public static boolean hasM(){
+    public static boolean hasM() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
     }
 
-
+    /**
+     * 当前是否在7.0以上
+     */
+    public static boolean hasN() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N;
+    }
 }
---
+++ libFrame/src/main/java/com/zcolin/frame/util/HtmlUtil.java
@@ -0,0 +1,91 @@
+/*
+ * *********************************************************
+ *   author   colin
+ *   company  fosung
+ *   email    wanglin2046@126.com
+ *   date     17-9-6 下午4:52
+ * ********************************************************
+ */
+
+package com.zcolin.frame.util;
+
+import android.os.Build;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+
+import static android.text.Html.FROM_HTML_MODE_LEGACY;
+import static android.text.Html.TO_HTML_PARAGRAPH_LINES_CONSECUTIVE;
+
+/**
+ *
+ */
+public class HtmlUtil {
+    /**
+     * 默认的fromHtml会增加两个空格，此函数会将默认增加的空格去掉
+     */
+    public static Spanned fromHtml(String strHtml) {
+        if (strHtml == null) {
+            return null;
+        }
+
+        Spanned spanned;
+        if (Build.VERSION.SDK_INT >= 24) {
+            spanned = Html.fromHtml(strHtml, FROM_HTML_MODE_LEGACY);
+        } else {
+            spanned = Html.fromHtml(strHtml);
+        }
+
+        SpannableStringBuilder builder;
+        if (spanned instanceof SpannableStringBuilder) {
+            builder = (SpannableStringBuilder) spanned;
+        } else {
+            builder = new SpannableStringBuilder(spanned);
+        }
+
+        if (builder.length() > 2 && "\n\n".equals(builder.subSequence(builder.length() - 2, builder.length())
+                                                         .toString())) {
+            builder.delete(builder.length() - 2, builder.length());
+        }
+
+        return builder;
+    }
+
+    /**
+     * 默认的toHtml会增加一个空格，此函数会将默认增加的空格去掉
+     */
+    public static String toHtml(Spanned spanned) {
+        String strHtmlContent;
+        if (Build.VERSION.SDK_INT >= 24) {
+            strHtmlContent = Html.toHtml(spanned, TO_HTML_PARAGRAPH_LINES_CONSECUTIVE)
+                                 .replace(" dir=\"ltr\"", "");
+        } else {
+            strHtmlContent = Html.toHtml(spanned)
+                                 .replace(" dir=\"ltr\"", "");
+        }
+
+        if (strHtmlContent.endsWith("\n")) {
+            strHtmlContent = strHtmlContent.substring(0, strHtmlContent.length() - 1);
+        }
+        return strHtmlContent;
+    }
+
+    /**
+     * 去除所有html标签和换行空格等符号
+     */
+    public static String fromHtmlWithoutHtml(String str) {
+        if (str == null) {
+            return null;
+        }
+
+        if (Build.VERSION.SDK_INT >= 24) {
+            return Html.fromHtml(str, FROM_HTML_MODE_LEGACY)
+                       .toString()
+                       .replaceAll("\\s*", "");
+        } else {
+            return Html.fromHtml(str)
+                       .toString()
+                       .replaceAll("\\s*", "");
+        }
+    }
+}
