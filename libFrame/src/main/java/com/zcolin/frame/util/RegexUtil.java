ca880a335d3938d60bb926c2e12a9de560336707
zcolin <57521534@qq.com>
2017-12-27 14:54:18 +0800

    # 手机正则更新

--- libFrame/src/main/java/com/zcolin/frame/util/RegexUtil.java
+++ libFrame/src/main/java/com/zcolin/frame/util/RegexUtil.java
@@ -90,7 +90,7 @@ public final class RegexUtil {
      * @return 验证成功返回true，验证失败返回false
      */
     public static boolean isMobile(String mobile) {
-        String regex = "(\\+\\d+)?1[34578]\\d{9}$";
+        String regex = "(\\+\\d+)?1[345789]\\d{9}$";
         return Pattern.matches(regex, mobile);
     }
 
